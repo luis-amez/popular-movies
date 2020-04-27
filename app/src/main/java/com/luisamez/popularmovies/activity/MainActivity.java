20c0f8b2dadcb17b94ae842c2813b6191ca12324
Luis Amez <luis.amez.vitoria@gmail.com>
2020-04-27 18:32:44 +0200

    Add error message for when the API call fails

--- app/src/main/java/com/luisamez/popularmovies/activity/MainActivity.java
+++ app/src/main/java/com/luisamez/popularmovies/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.luisamez.popularmovies.BuildConfig;
 import com.luisamez.popularmovies.R;
@@ -32,12 +33,14 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
     private MovieDBAPIService service;
     private List<Movie> movies;
     private ProgressBar progressBar;
+    private TextView errorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         progressBar = findViewById(R.id.progress_circular);
+        errorView = findViewById(R.id.error_message);
         moviesGrid = findViewById(R.id.movies_grid);
         service = RetrofitInstance.getRetrofitInstance().create(MovieDBAPIService.class);
         showMostPopularMovies();
@@ -51,8 +54,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        moviesGrid.setVisibility(View.GONE);
-        progressBar.setVisibility(View.VISIBLE);
+        showProgressBar();
         if (item.getItemId() == R.id.action_most_popular) {
             showMostPopularMovies();
             return true;
@@ -72,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
         }
     }
 
-    public void showMostPopularMovies() {
+    private void showMostPopularMovies() {
         Call<APICallResults> call = service.getPopularMovies(BuildConfig.THE_MOVIE_DB_API_KEY);
         call.enqueue(new Callback<APICallResults>() {
             @Override
@@ -83,12 +85,12 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
 
             @Override
             public void onFailure(Call<APICallResults> call, Throwable t) {
-
+                showErrorMessage();
             }
         });
     }
 
-    public void showTopRatedMovies() {
+    private void showTopRatedMovies() {
         Call<APICallResults> call = service.getTopRatedMovies(BuildConfig.THE_MOVIE_DB_API_KEY);
         call.enqueue(new Callback<APICallResults>() {
             @Override
@@ -99,16 +101,33 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
 
             @Override
             public void onFailure(Call<APICallResults> call, Throwable t) {
-
+                showErrorMessage();
             }
         });
     }
 
-    public void setRecyclerView(List<Movie> movies) {
-        progressBar.setVisibility(View.GONE);
-        moviesGrid.setVisibility(View.VISIBLE);
+    private void setRecyclerView(List<Movie> movies) {
         moviesGrid.setHasFixedSize(true);
         adapter = new MoviesAdapter(movies, /* onClickListener */ this);
         moviesGrid.setAdapter(adapter);
+        showMoviesGrid();
+    }
+
+    private void showMoviesGrid() {
+        progressBar.setVisibility(View.GONE);
+        errorView.setVisibility(View.GONE);
+        moviesGrid.setVisibility(View.VISIBLE);
+    }
+
+    private void showProgressBar() {
+        errorView.setVisibility(View.GONE);
+        moviesGrid.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        progressBar.setVisibility(View.GONE);
+        moviesGrid.setVisibility(View.GONE);
+        errorView.setVisibility(View.VISIBLE);
     }
 }
